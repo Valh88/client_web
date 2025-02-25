8100a7cc3082641f886b9a3f8a7f964b7b97a8ba
Vano <simplmd@gmail.com>
2025-02-26 03:10:18 +0700

    view

--- src/Controller.hx
+++ src/Controller.hx
@@ -10,20 +10,40 @@ class Controller
 
 	public function new()
 	{
-        _socket = new WebSocketClient(null);
+		_socket = new WebSocketClient(null);
 		_httpClient = new RegistarionAutentication(_socket);
-        _httpClient.runSocket = function(data) {
-            _socket = new WebSocketClient(data);
-            _socket.connect();
-        }
-        _view = new View(false);
-        _view.loginFunc = _httpClient.login;
-        _view.deleteMe = _httpClient.deleteMe;
+		_httpClient.runSocket = function(data)
+		{
+			_socket = new WebSocketClient(data);
+
+			_socket.addContacts = function(data) {
+				trace(data.contacts);
+				_view.contacts = data.contacts;
+				_view.renderContacts();
+			}
+
+			_socket.addMessages = function(data) {
+				// _view.messages.push(data.new_messages);
+				var array = data.new_messages.concat(_view.messages);
+				trace(array);
+				trace("123123123");
+				trace(data);
+				_view.messages = array;
+				_view.renderMessages();
+			}
+
+			_socket.connect();
+			_view.socket = _socket;
+		}
+		_view = new View(false);
+		_view.loginFunc = _httpClient.login;
+		_view.deleteMe = _httpClient.deleteMe;
+		_view.getMe = _httpClient.getMe;
 	}
 
 	public function run():Void
 	{
 		_view.onStart();
-        _view.render();
+		_view.render();
 	}
 }
